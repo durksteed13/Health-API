31e410a900e7697e9b109cba56f89486cc5f2d2f
Durk Steed <durksteed13@gmail.com>
2020-11-21 00:42:52 -0500

    user saved searches functionality

--- webapp/index.js
+++ webapp/index.js
@@ -1,6 +1,7 @@
 var loginForm = "<div class='card-dynamic' style='height: 350px; max-height: 350px;'><div class='card-title red-light'><h2 class='is-indent-max'>Login</h2><button onclick='closePopup()' class='is-light red-light is-right-indent no-hover'>X</button></div><div class='message-container red-light'></div><div class='card-content'><form class='popup-form' method='post'><input required name='username' type='username' placeholder='username' class='red-light'><input required name='password' type='password' placeholder='password' class='red-light'><div id='btn-wrapper'><button type='button' id='btn-popup' class='red-dark is-large' onclick='handleLogIn()'>Login</button></div></form></div></div>";
 var signupForm = "<div class='card-dynamic-tall' style='height: 475px; max-height: 475px;'><div class='card-title red-light'><h2 class='is-indent-max'>Sign Up</h2><button onclick='closePopup()' class='is-light red-light is-right-indent no-hover'>X</button></div><div class='message-container red-light'></div><div class='card-content'><form class='popup-form'><input required name='name' type='name' placeholder='name' class='red-light' method='post'><input required name='email' type='email' placeholder='email address' class='red-light'><input required name='username' type='username' placeholder='username' class='red-light'><input required name='password' type='password' placeholder='password' class='red-light'><div id='btn-wrapper'><button type='button' id='btn-popup' class='red-dark is-large' onclick='handleSignUp()'>Sign Up</button></div></form></div></div>";
 var apis = [];
+var saved = 0;
 
 function signUpForm() {
 	$(".popup-display").append(signupForm);
@@ -40,15 +41,16 @@ async function handleSignUp() {
       });
 	var success = result['data']['success'];
     if(success) {
-    	console.log(result);
     	var username = result['data']['username'];
     	var userID = result['data']['id'];
     	var role = result['data']['role'];
+    	var name = result['data']['name'];
 
     	localStorage.setItem('loggedIn', true);
     	localStorage.setItem('username', username);
     	localStorage.setItem('id', userID);
     	localStorage.setItem('role', role);
+    	localStorage.setItem('name', name);
     	location.reload();
     } else {
 		$('.message-container').prepend("<div class='red-light'>Username already taken, please try another username<div>");
@@ -77,11 +79,13 @@ async function handleLogIn() {
     	var username = result['data']['result'][0]['username'];
     	var userID = result['data']['result'][0]['id'];
     	var role = result['data']['result'][0]['role'];
+    	var name = result['data']['result'][0]['name'];
 
     	localStorage.setItem('loggedIn', true);
     	localStorage.setItem('username', username);
     	localStorage.setItem('id', userID);
     	localStorage.setItem('role', role);
+    	localStorage.setItem('name', name);
     	location.reload();
     } else {
     	$('.message-container').prepend("<div class='red-light'>Invalid username or password, please try again<div>");
@@ -97,12 +101,15 @@ function checkLogIn() {
 	if(localStorage.getItem('loggedIn')) {
 		$(".btn").remove();
 		$("#sign-up-link").remove();
+		$('#welcome').append("Welcome back, " + localStorage.getItem('name') + "!");
 		var role = localStorage.getItem('role');
+		loadSearches();
 		if(role == 1) {
 			$('#landing-navigation-container').append("<a href='admin.html'><button id='btn-log' class='is-light red-light user-btn' onclick=''>Admin Page</button></a>");
 		}
 	} else {
 		$(".user-btn").remove();
+		$('#welcome').remove();
 	}
 }
 
@@ -129,6 +136,7 @@ $('#api-selector').change(async function () {
        		api : selected
        	}
     });
+    console.log('here');
     // $('#param-selector').append("<option value='' disabled selected>Select API Search Parameters</option>");
   	result['data'].forEach(param => {
   		var paramName = param['name'];
@@ -157,8 +165,72 @@ async function handleAPISubmit() {
    	$('#data-container').append(JSON.stringify(APIResult['data']));
 }
 
+async function handleSearchSubmit(url, selectedAPI, selectedParam) {
+	$('#data-container').empty();
+	$('#data-search').empty();
+	$('#data-search').empty();
+	$('#data-search').append("Data from " + selectedAPI + ", " + selectedParam);
+	const APIResult = await axios({
+        method: 'get',
+       	url: url
+    });
+   	$('#data-container').append(JSON.stringify(APIResult['data']));
+}
+
+async function loadSearches() {
+	const result = await axios({
+        method: 'post',
+       	url: "http://localhost:3000/usersSearches/" + localStorage.getItem('id'),
+       	data: {
+       		userID : localStorage.getItem('id')
+       	}
+    });
+    saved = result['data'].length;
+    result['data'].forEach(search => {
+    	var api = search['api'];
+    	var param = search['param'];
+    	var url = search['url'];
+    	$('#saves').append(`<div class='search-option'><div class='search-option-desc'>`+param+` from `+api+`</div><div class='search-btn' onclick='handleSearchSubmit("`+url+`", "`+api+`", "`+param+`")'>Get Data</div><div>`);
+    });
+}
+
+function loadSelect() {
+	$('#landing-wrapper').css('height', '150px');
+	$('#search-container').css("visibility", "visible");
+	$('#search-container').css("height", "auto");
+	$('#search-container').css("overflow", "normal");
+	$('#save-container').css("visibility", "hidden");
+	$('#save-container').css("height", "1px");
+	$('#save-container').css("overflow", "scroll");
+	$("#btn-log-save").removeClass("red-dark");
+	$("#btn-log-save").addClass("red-light");
+	$("#btn-log-save").addClass("is-light");
+
+	$("#btn-log-select").addClass("red-dark");
+	$("#btn-log-select").addClass("red-light");
+	$("#btn-log-select").removeClass("is-light");
+}
+
+function loadSaved() {
+	if(saved > 3) {
+    	$('#landing-wrapper').css('height', 'auto');
+    }
+	$('#search-container').css("visibility", "hidden");
+	$('#search-container').css("height", "1px");
+	$('#search-container').css("overflow", "scroll");
+	$('#save-container').css("visibility", "visible");
+	$('#save-container').css("height", "auto");
+	$('#save-container').css("overflow", "normal");
+	$("#btn-log-select").removeClass("red-dark");
+	$("#btn-log-select").addClass("red-light");
+	$("#btn-log-select").addClass("is-light");
+
+	$("#btn-log-save").addClass("red-dark");
+	$("#btn-log-save").addClass("red-light");
+	$("#btn-log-save").removeClass("is-light");
+}
+
 $(document).ready(function() {
 	checkLogIn();
 	loadAPI();
-	console.log(apis);
 });
